9bba510b4696640cd5efb386947ced8923dcf2cb
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-07-20 01:43:04 +0000

    final

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def hash(wordlistpassword):
 def bruteforce(guesspasswordlist, actual_password_hash):
   for guess_password in guesspasswordlist:
     if hash(guess_password) == actual_password_hash:
-      print("Hey! your password is:", guess_password, 
-            "Congrats! I have cracked your password, Please change it")
+      print("HA! your password is:", guess_password, 
+            "HAHHAHAHA Your password is too easy, Please change it")
       exit()
 url = 'https://raw.githubusercontent.com/berzerk0/Probable-Wordlists/master/Real-Passwords/Top12Thousand-probable-v2.txt'
 
@@ -30,4 +30,4 @@ guesspasswordlist = wordlist.split('\n')
 
 bruteforce(guesspasswordlist, actual_password_hash)
 
-print("Hey! I couldn't crack your password, as it was not in the wordlist, YOU WIN!!!")
+print("Hey! I couldn't crack your password, as it was not in the wordlist, YOU WIN!!")
